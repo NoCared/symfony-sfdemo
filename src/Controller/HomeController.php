780ccd52e5c83e0cd2bc60020b61f0d2121e6205
nocared <paul.richarddu30@gmail.com>
2023-11-24 16:29:07 +0100

    Wip

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -8,12 +8,32 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
+    public $users = [
+        [
+        'nom' => 'Paulo Biscotto',
+        'email' => 'pauloleboss@gmail.com',
+        'age' => 23
+        ],
+        [
+        'nom' => 'Bla blo',
+        'email' => 'blabla@free.fr',
+        'age' => 54
+        ],
+        [
+        'nom' => 'Fee Lation',
+        'email' => 'negiubrengeniugrb@zerjhzerjerjutrezrztjrj.fr',
+        'age' => 99
+        ]
+    ];
+
+
     #[Route('/', name: 'app_home')]
     public function index(): Response
     {
         return $this->render('home/index.html.twig', [
             'titre' => 'Titre : Demo Symfony',
-            'nom' => 'Paulo Biscotto'
+            'nom' => 'Paulo Biscotto',
+            'users' => $this->users
         ]);
     }
 }
