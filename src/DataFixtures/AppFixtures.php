1280702c123b87517b6a09740532e0ef92ededd8
nocared <paul.richarddu30@gmail.com>
2023-12-01 11:22:23 +0100

    ADD - Datas with faker

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,16 +2,25 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Formation;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
 
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $faker = Factory::create('fr_FR');
+        for ($i = 0; $i < 50; $i++) {
+            $formation = new Formation();
 
+            $formation->setNom($faker->words(3, true));
+            $formation->setDescription($faker->paragraph(20, true));
+            $formation->setLieux($faker->city());
+
+            $manager->persist($formation);
+        }
         $manager->flush();
     }
 }
